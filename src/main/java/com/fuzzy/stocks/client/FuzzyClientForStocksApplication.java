006ef9f05dee858803de46204b617f905a9bcd01
erencan <erencan@netas.com.tr>
2016-11-14 19:46:40 +0300

    first commit

---
+++ src/main/java/com/fuzzy/stocks/client/FuzzyClientForStocksApplication.java
@@ -0,0 +1,12 @@
+package com.fuzzy.stocks.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzyClientForStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzyClientForStocksApplication.class, args);
+	}
+}
